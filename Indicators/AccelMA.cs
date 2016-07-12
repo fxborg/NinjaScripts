f7879744ea94745e0ac4e81046222ba84f1b541e
fxborg <aulu.aulu@yahoo.com>
2016-07-13 01:26:23 +0900

    Update AccelMA.cs

--- Indicators/AccelMA.cs
+++ Indicators/AccelMA.cs
@@ -27,123 +27,123 @@ using NinjaTrader.NinjaScript.DrawingTools;
 //This namespace holds indicators in this folder and is required. Do not change it.
 namespace NinjaTrader.NinjaScript.Indicators
 {
-	/// <summary>
-	/// Accel Moving Average
-	/// </summary>
-	public class AccelMA : Indicator
-	{
-		private Series<double>		_mom;
-		private Series<double>		_volat;
-		private Series<double>		_ma;
-		private List<double>        _accel;
-		private int					_accelPeriod;
-		private double				_coef1,_coef2,_coef3;
-		
-		
-		protected override void OnStateChange()
-		{
-			if (State == State.SetDefaults)
-			{
-				//---ÉpÉâÉÅÅ[É^ÇÃÉfÉtÉHÉãÉgílÇê›íËÅB
-				Description					= "Accel Moving Average";
-				Name						= "AccelMA";
-				IsSuspendedWhileInactive	= true;
-				IsOverlay					= true;
-				AccelSpeed					= 0.45;
-				Period						= 24;
-				Smoothing					= 10;
+    /// <summary>
+    /// Accel Moving Average
+    /// </summary>
+    public class AccelMA : Indicator
+    {
+        private Series<double>        _mom;
+        private Series<double>        _volat;
+        private Series<double>        _ma;
+        private List<double>        _accel;
+        private int                    _accelPeriod;
+        private double                _coef1,_coef2,_coef3;
+        
+        
+        protected override void OnStateChange()
+        {
+            if (State == State.SetDefaults)
+            {
+                //---„Éë„É©„É°„Éº„Çø„ÅÆ„Éá„Éï„Ç©„É´„ÉàÂÄ§„ÇíË®≠ÂÆö„ÄÇ
+                Description                    = "Accel Moving Average";
+                Name                        = "AccelMA";
+                IsSuspendedWhileInactive    = true;
+                IsOverlay                    = true;
+                AccelSpeed                    = 0.45;
+                Period                        = 24;
+                Smoothing                    = 10;
 
-				AddPlot(Brushes.Orange, "Accel Moving Average");
-			}
-			else if (State == State.Configure)
-			{
-				//--- Ç±Ç±ÇÕÉZÉbÉVÉáÉìäJénéûÇ…àÍìxÇæÇØé¿çsÇ≥ÇÍÇÈÅB
-				_ma					= new Series<double>(this);
-				_mom					= new Series<double>(this);
-				_volat				= new Series<double>(this);
-				_accel				= new List<double>();
-				//--- ÉAÉNÉZÉãÉtÉBÉãÉ^
-				_accelPeriod=(int)(AccelSpeed*15.0);
-			    for(int j=0;j<Period;j++) _accel.Add(Math.Pow(AccelSpeed,Math.Log(j+1)));
-				//--- ÉXÉÄÅ[ÉWÉìÉOÉtÉBÉãÉ^
-				double sq2= Math.Sqrt(2.0);
-				double a = Math.Exp( -sq2 * Math.PI / Smoothing );
-				_coef2 = 2.0 * a * Math.Cos(sq2 * Math.PI / Smoothing);
-  			    _coef3 = -a * a;
-			    _coef1 = 1.0 - _coef2 - _coef3;
-				//---
-				}
-		}
-		
-		protected override void OnBarUpdate()
-		{
-			if(CurrentBar<1)
-		  	{	//--- 1ÉoÅ[Ç…ñûÇΩÇ»Ç¢èÍçá
-		  		_mom[0]=0.0;
-		    	_volat[0]=0.0;
-				_ma[0]=Close[0];
-				Main[0]=Close[0];
-		  		return;
-		  	}
-			//--- äKç∑Ç∆äKç∑ÇÃê‚ëŒílÇÉZÉbÉg
-		  	_mom[0]=Close[0]-Close[1];
-      	  	_volat[0]=Math.Abs(Close[0]-Close[1]);
+                AddPlot(Brushes.Orange, "Accel Moving Average");
+            }
+            else if (State == State.Configure)
+            {
+                //--- „Åì„Åì„ÅØ„Çª„ÉÉ„Ç∑„Éß„É≥ÈñãÂßãÊôÇ„Å´‰∏ÄÂ∫¶„Å†„ÅëÂÆüË°å„Åï„Çå„Çã„ÄÇ
+                _ma                    = new Series<double>(this);
+                _mom                    = new Series<double>(this);
+                _volat                = new Series<double>(this);
+                _accel                = new List<double>();
+                //--- „Ç¢„ÇØ„Çª„É´„Éï„Ç£„É´„Çø
+                _accelPeriod=(int)(AccelSpeed*15.0);
+                for(int j=0;j<Period;j++) _accel.Add(Math.Pow(AccelSpeed,Math.Log(j+1)));
+                //--- „Çπ„É†„Éº„Ç∏„É≥„Ç∞„Éï„Ç£„É´„Çø
+                double sq2= Math.Sqrt(2.0);
+                double a = Math.Exp( -sq2 * Math.PI / Smoothing );
+                _coef2 = 2.0 * a * Math.Cos(sq2 * Math.PI / Smoothing);
+                  _coef3 = -a * a;
+                _coef1 = 1.0 - _coef2 - _coef3;
+                //---
+            }
+        }
+        
+        protected override void OnBarUpdate()
+        {
+            if(CurrentBar<1)
+              { //--- 1„Éê„Éº„Å´Ê∫Ä„Åü„Å™„ÅÑÂ†¥Âêà
+                _mom[0]=0.0;
+                _volat[0]=0.0;
+                _ma[0]=Close[0];
+                Main[0]=Close[0];
+                return;
+              }
+            //--- ÈöéÂ∑Æ„Å®ÈöéÂ∑Æ„ÅÆÁµ∂ÂØæÂÄ§„Çí„Çª„ÉÉ„Éà
+            _mom[0]=Close[0]-Close[1];
+            _volat[0]=Math.Abs(Close[0]-Close[1]);
 
-			if(CurrentBar<Math.Max(_accelPeriod,Period))
-			{	//--- åvéZä˙ä‘Ç…ñûÇΩÇ»Ç¢èÍçá
-				_ma[0]=Close[0];				
-				Main[0]=Close[0];
-				return;
-			}
-			//--- â¡ë¨ÉtÉBÉãÉ^ÇÃåvéZ
-      		double dsum=0.0000000001,volat=0.0000000001,dmax=0.0, dmin=0.0;
-			for(int j=0;j<_accelPeriod;j++)
-        	{	     		
-         		dsum += _mom[j] *_accel[j];
-         		if(dsum>dmax)dmax=dsum;
-         		if(dsum<dmin)dmin=dsum;
-        	}
-	        //--- ä˙ä‘ì‡É{ÉâÉeÉBÉäÉeÉBÇåvéZ
-    	    for(int j=0;j<Period;j++) volat+=_volat[j];
-			
-      	Å@Å@ double range=Math.Max(0.0000000001,dmax-dmin);
-      	Å@Å@ double accel1=range/volat;		
-			//--- AccelMAÇåvéZ
-            _ma[0]=accel1*(Close[0]-_ma[1])+_ma[1];      		
-			//--- Ç≥ÇÁÇ…ÉXÉÄÅ[ÉWÉìÉO
-			Main[0]= _coef1*_ma[0]+_coef2*Main[1]+_coef3*Main[2];
-		}
+            if(CurrentBar<Math.Max(_accelPeriod,Period))
+            {    //--- Ë®àÁÆóÊúüÈñì„Å´Ê∫Ä„Åü„Å™„ÅÑÂ†¥Âêà
+                _ma[0]=Close[0];                
+                Main[0]=Close[0];
+                return;
+            }
+            //--- Âä†ÈÄü„Éï„Ç£„É´„Çø„ÅÆË®àÁÆó
+            double dsum=0.0000000001,volat=0.0000000001,dmax=0.0, dmin=0.0;
+            for(int j=0;j<_accelPeriod;j++)
+            {                 
+                 dsum += _mom[j] *_accel[j];
+                 if(dsum>dmax)dmax=dsum;
+                 if(dsum<dmin)dmin=dsum;
+            }
+            //--- ÊúüÈñìÂÜÖ„Éú„É©„ÉÜ„Ç£„É™„ÉÜ„Ç£„ÇíË®àÁÆó
+            for(int j=0;j<Period;j++) volat+=_volat[j];
+            
+            double range=Math.Max(0.0000000001,dmax-dmin);
+            double accel1=range/volat;        
+            //--- AccelMA„ÇíË®àÁÆó
+            _ma[0]=accel1*(Close[0]-_ma[1])+_ma[1];              
+            //--- „Åï„Çâ„Å´„Çπ„É†„Éº„Ç∏„É≥„Ç∞
+            Main[0]= _coef1*_ma[0]+_coef2*Main[1]+_coef3*Main[2];
+        }
 
-		#region Properties
-		//ÉCÉìÉWÉPÅ[É^Å[ÉoÉbÉtÉ@
-		[Browsable(false)]
-		[XmlIgnore()]
-		public Series<double> Main
-		{
-			get { return Values[0]; }
-		}		
-		// ÉpÉâÉÅÅ[É^ ÉAÉNÉZÉãÉXÉsÅ[Éh 
-		[Range(0.1, 0.9), NinjaScriptProperty]
-		[Display(ResourceType = typeof(Custom.Resource), Name = "AccelSpeed",
-								GroupName = "NinjaScriptParameters", Order = 1)]
-		public double AccelSpeed
-		{ get; set; }
+        #region Properties
+        //„Ç§„É≥„Ç∏„Ç±„Éº„Çø„Éº„Éê„ÉÉ„Éï„Ç°
+        [Browsable(false)]
+        [XmlIgnore()]
+        public Series<double> Main
+        {
+            get { return Values[0]; }
+        }        
+        // „Éë„É©„É°„Éº„Çø „Ç¢„ÇØ„Çª„É´„Çπ„Éî„Éº„Éâ 
+        [Range(0.1, 0.9), NinjaScriptProperty]
+        [Display(ResourceType = typeof(Custom.Resource), Name = "AccelSpeed",
+                                GroupName = "NinjaScriptParameters", Order = 1)]
+        public double AccelSpeed
+        { get; set; }
 
-		// ÉpÉâÉÅÅ[É^ ä˙ä‘ 
-		[Range(1, int.MaxValue), NinjaScriptProperty]
-		[Display(ResourceType = typeof(Custom.Resource), Name = "Period", 
-								GroupName = "NinjaScriptParameters", Order = 2)]
-		public int Period
-		{ get; set; }
+        // „Éë„É©„É°„Éº„Çø ÊúüÈñì 
+        [Range(1, int.MaxValue), NinjaScriptProperty]
+        [Display(ResourceType = typeof(Custom.Resource), Name = "Period", 
+                                GroupName = "NinjaScriptParameters", Order = 2)]
+        public int Period
+        { get; set; }
 
-		// ÉpÉâÉÅÅ[É^ ÉXÉÄÅ[ÉWÉìÉO
-		[Range(1, int.MaxValue), NinjaScriptProperty]
-		[Display(ResourceType = typeof(Custom.Resource), Name = "Smoothing",
-								GroupName = "NinjaScriptParameters", Order = 3)]
-		public int Smoothing
-		{ get; set; }
-		#endregion
-	}
+        // „Éë„É©„É°„Éº„Çø „Çπ„É†„Éº„Ç∏„É≥„Ç∞
+        [Range(1, int.MaxValue), NinjaScriptProperty]
+        [Display(ResourceType = typeof(Custom.Resource), Name = "Smoothing",
+                                GroupName = "NinjaScriptParameters", Order = 3)]
+        public int Smoothing
+        { get; set; }
+        #endregion
+    }
 }
 
 
@@ -151,55 +151,55 @@ namespace NinjaTrader.NinjaScript.Indicators
 
 namespace NinjaTrader.NinjaScript.Indicators
 {
-	public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
-	{
-		private AccelMA[] cacheAccelMA;
-		public AccelMA AccelMA(double accelSpeed, int period, int smoothing)
-		{
-			return AccelMA(Input, accelSpeed, period, smoothing);
-		}
+    public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
+    {
+        private AccelMA[] cacheAccelMA;
+        public AccelMA AccelMA(double accelSpeed, int period, int smoothing)
+        {
+            return AccelMA(Input, accelSpeed, period, smoothing);
+        }
 
-		public AccelMA AccelMA(ISeries<double> input, double accelSpeed, int period, int smoothing)
-		{
-			if (cacheAccelMA != null)
-				for (int idx = 0; idx < cacheAccelMA.Length; idx++)
-					if (cacheAccelMA[idx] != null && cacheAccelMA[idx].AccelSpeed == accelSpeed && cacheAccelMA[idx].Period == period && cacheAccelMA[idx].Smoothing == smoothing && cacheAccelMA[idx].EqualsInput(input))
-						return cacheAccelMA[idx];
-			return CacheIndicator<AccelMA>(new AccelMA(){ AccelSpeed = accelSpeed, Period = period, Smoothing = smoothing }, input, ref cacheAccelMA);
-		}
-	}
+        public AccelMA AccelMA(ISeries<double> input, double accelSpeed, int period, int smoothing)
+        {
+            if (cacheAccelMA != null)
+                for (int idx = 0; idx < cacheAccelMA.Length; idx++)
+                    if (cacheAccelMA[idx] != null && cacheAccelMA[idx].AccelSpeed == accelSpeed && cacheAccelMA[idx].Period == period && cacheAccelMA[idx].Smoothing == smoothing && cacheAccelMA[idx].EqualsInput(input))
+                        return cacheAccelMA[idx];
+            return CacheIndicator<AccelMA>(new AccelMA(){ AccelSpeed = accelSpeed, Period = period, Smoothing = smoothing }, input, ref cacheAccelMA);
+        }
+    }
 }
 
 namespace NinjaTrader.NinjaScript.MarketAnalyzerColumns
 {
-	public partial class MarketAnalyzerColumn : MarketAnalyzerColumnBase
-	{
-		public Indicators.AccelMA AccelMA(double accelSpeed, int period, int smoothing)
-		{
-			return indicator.AccelMA(Input, accelSpeed, period, smoothing);
-		}
+    public partial class MarketAnalyzerColumn : MarketAnalyzerColumnBase
+    {
+        public Indicators.AccelMA AccelMA(double accelSpeed, int period, int smoothing)
+        {
+            return indicator.AccelMA(Input, accelSpeed, period, smoothing);
+        }
 
-		public Indicators.AccelMA AccelMA(ISeries<double> input , double accelSpeed, int period, int smoothing)
-		{
-			return indicator.AccelMA(input, accelSpeed, period, smoothing);
-		}
-	}
+        public Indicators.AccelMA AccelMA(ISeries<double> input , double accelSpeed, int period, int smoothing)
+        {
+            return indicator.AccelMA(input, accelSpeed, period, smoothing);
+        }
+    }
 }
 
 namespace NinjaTrader.NinjaScript.Strategies
 {
-	public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
-	{
-		public Indicators.AccelMA AccelMA(double accelSpeed, int period, int smoothing)
-		{
-			return indicator.AccelMA(Input, accelSpeed, period, smoothing);
-		}
+    public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
+    {
+        public Indicators.AccelMA AccelMA(double accelSpeed, int period, int smoothing)
+        {
+            return indicator.AccelMA(Input, accelSpeed, period, smoothing);
+        }
 
-		public Indicators.AccelMA AccelMA(ISeries<double> input , double accelSpeed, int period, int smoothing)
-		{
-			return indicator.AccelMA(input, accelSpeed, period, smoothing);
-		}
-	}
+        public Indicators.AccelMA AccelMA(ISeries<double> input , double accelSpeed, int period, int smoothing)
+        {
+            return indicator.AccelMA(input, accelSpeed, period, smoothing);
+        }
+    }
 }
 
 #endregion
